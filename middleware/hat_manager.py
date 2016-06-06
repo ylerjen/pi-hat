71529679dfbac3cf8931953a8cfee5caeceb6d1e
Yann Lerjen <yann.lerjen@gmail.com>
2016-06-06 22:05:29 +0200

    create sense manager

---
+++ middleware/hat_manager.py
@@ -0,0 +1,25 @@
+class HatManager(object):
+
+    def __init__(self, sense):
+        self.sense = sense
+
+        self._pressure = self.sense.get_pressure()
+        self._temperature = self.sense.get_temperature()
+        self._humidity = self.sense.get_humidity()
+
+    def refresh_state(self):
+        self._pressure = self.sense.get_pressure()
+        self._temperature = self.sense.get_temperature()
+        self._humidity = self.sense.get_humidity()
+
+    @property
+    def get_humidity(self):
+        return self._humidity
+
+    @property
+    def get_temperature(self):
+        return self._temperature
+
+    @property
+    def get_pressure(self):
+        return self._pressure
