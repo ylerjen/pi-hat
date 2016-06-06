6988cc4233f104c293e7e00cc085d36df79f2bca
Yann Lerjen <yann.lerjen@gmail.com>
2016-06-06 22:29:30 +0200

    add a route for sense hat text display

--- middleware/app.py
+++ middleware/app.py
@@ -1,8 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from sense_hat import SenseHat
 from hat_manager import HatManager
 
 app = Flask(__name__)
+
+sense_hat = SenseHat()
+hat_manager = HatManager(sense_hat)
 
 
 @app.route('/')
@@ -12,10 +15,19 @@ def index():
 
 @app.route('/status')
 def get_status():
-    sense_hat = SenseHat()
-    hat_manager = HatManager(sense_hat)
     status = {'pressure': hat_manager.get_pressure, 'temperature': hat_manager.get_temperature, 'humidity': hat_manager.get_humidity}
     return jsonify({'status': status})
+
+
+@app.route('/message', methods=['POST'])
+def print_message():
+    if not request.json or not 'message' in request.json:
+        abort(400)
+    message = request.json['message']
+    color = request.json['text_color']
+    bg_color = request.json['bg_color']
+    hat_manager.set_message(message)
+
 
 if __name__ == '__main__':
     # 0.0.0.0 = accessible to any device on the network
