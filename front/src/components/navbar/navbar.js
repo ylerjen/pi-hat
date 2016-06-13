d130da5c6b9ce50cb373da016ad0dce7821a6e6e
Yann Lerjen <yann.lerjen@gmail.com>
2016-06-13 22:56:05 +0200

    change logo and add nav selection

--- front/src/components/navbar/navbar.js
+++ front/src/components/navbar/navbar.js
@@ -1,19 +1,33 @@
 import React from 'react';
 
-console.info('Navbar');
+const DASHBOARD = 'dashboard';
+const SPEECH = 'speech';
+const ABOUT = 'about';
 
 export default class Navbar extends React.Component {
-  render () {
-    return (
-        <nav className="navbar navbar-light bg-faded">
-            <div className="nav navbar-nav">
-                <a className="nav-item nav-link active" href="#">Dashboard <span className="sr-only">(current)</span></a>
-                <a className="nav-item nav-link" href="#">Speaker</a>
-                <a className="nav-item nav-link" href="#">About</a>
-            </div>
-        </nav>
-    );
-  }
+    constructor (props) {        
+        super(props);
+        var _self = this;
+        this.state = {currentPage: DASHBOARD};
+    }
+    navigateTo (pageName, proxy, other, evt) {
+        this.setState({ currentPage: pageName });
+    }
+    render () {
+        return (
+            <header className="app-header navbar navbar-dark bg-inverse">
+                <span className="navbar-brand"
+                    ><img src="dist/assets/img/pirhat-logo.svg" title="PiπHat"/
+                    ><span>PiπHat</span
+                ></span>
+                <nav className="nav navbar-nav">
+                    <a className={"nav-item nav-link "+ (this.state.currentPage === DASHBOARD ? 'active' : '')} href={"#"+DASHBOARD} onClick={this.navigateTo.bind(this,DASHBOARD)}>Dashboard</a>
+                    <a className={"nav-item nav-link "+ (this.state.currentPage === SPEECH ? 'active' : '')} href={"#"+SPEECH} onClick={this.navigateTo.bind(this, SPEECH)}>Speech</a>
+                    <a className={"nav-item nav-link "+ (this.state.currentPage === ABOUT ? 'active' : '')} href={"#"+ABOUT} onClick={this.navigateTo.bind(this, ABOUT)}>About</a>
+                </nav>
+            </header>
+        );
+    }
 }
 
 module.exports = Navbar;
