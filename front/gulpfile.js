aa39956be1bfecc5246a91ac19dd413e16709016
Yann Lerjen <yann.lerjen@gmail.com>
2017-02-18 15:53:50 +0100

    minor changes : indentation

--- front/gulpfile.js
+++ front/gulpfile.js
@@ -1,6 +1,6 @@
 var gulp        = require('gulp');
 var sass        = require('gulp-sass');
-var concat = require('gulp-concat');
+var concat      = require('gulp-concat');
 var source      = require('vinyl-source-stream');
 var babelify    = require('babelify');
 var browserify  = require('browserify');
