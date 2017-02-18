162891e50b9719b04d260f3e66bab0437be77e2f
Yann Lerjen <yann.lerjen@gmail.com>
2017-02-18 15:54:26 +0100

    add about content

--- front/src/modules/About.js
+++ front/src/modules/About.js
@@ -3,6 +3,14 @@ import Navbar from '../components/navbar/Navbar';
 
 export default React.createClass({
   render() {
-    return <div><Navbar/>Routed About</div>
+    return <div>
+        <header>
+            <Navbar/>
+        </header>
+        <main className="container">
+            <h1>About</h1>
+            <p>The PirHat application is a simple app to interact with your Raspberry Pi Sense Hat. It provides informations from the sensors and allow you to interact with the LED display</p>
+        </main>
+        </div>
   }
 });
