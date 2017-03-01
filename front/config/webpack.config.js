e52e00dc90de34956b388191c2445143b997ba16
Yann Lerjen <yann.lerjen@gmail.com>
2017-03-01 22:14:29 +0100

    add missing loaders

--- front/config/webpack.config.js
+++ front/config/webpack.config.js
@@ -31,13 +31,11 @@ module.exports = env => {
         }],
       }, {
           test: /\.s[ac]ss$/,
-          use: [{
-              loader: "style-loader" // creates style nodes from JS strings
-          }, {
-              loader: "css-loader" // translates CSS into CommonJS
-          }, {
-              loader: "sass-loader" // compiles Sass to CSS
-          }]
+          use: [
+            "style-loader", // creates style nodes from JS strings
+            "css-loader", // translates CSS into CommonJS
+            "sass-loader" // compiles Sass to CSS
+          ]
       }]
     },
 
